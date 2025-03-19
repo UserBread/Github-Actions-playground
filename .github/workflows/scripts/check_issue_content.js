7136a9963de7270a3a90990dcd8b659013f03720
Peter Murphy <petermurphy5672@gmail.com>
2025-03-19 09:52:59 +0000

    Add RCA checks to reopen issue

--- .github/workflows/scripts/check_issue_content.js
+++ .github/workflows/scripts/check_issue_content.js
@@ -1,22 +1,31 @@
 module.exports = async ({ github, context }) => {
-    
     const issue = context.payload.issue;
     let commentBody = "";
     let reopenIssue = false;
-    // List of sections from issue template. This list must be kept up-to-date!
+    // All sections from issue form. This list must be kept up-to-date!
     const sections = [
         'Source','Customer case', 'Internal incident', 'Description', 'Steps to reproduce', 'Expected results',
         'Actual results', 'Screenshots', 'Add fiori tools component/version','Project Files', 'Operating System',
         'Browser', 'Environment', 'Problem', 'Why was it missed?', 'How can we avoid this'
     ]
+    // Required Root Cause Analysis Sections
+    const rca_sections = [
+        'Problem', 'Why was it missed?', 'How can we avoid this'
+    ]
 
-    // Loop through sections and test if a response was given
+    // Check all issue sections and test if a response was given
     for (let i=0; i<sections.length - 1; i++) {
         if (isBetween(issue.body, sections[i], "_No response_", sections[i+1])) {
             commentBody += `❌ ${sections[i]} section is missing from the issue.\n`
-            reopenIssue = true;
         } else {
             commentBody += `✅ ${sections[i]} section was included with the issue.\n`
+        }
+    }
+
+    // Reopen issue if required RCA section is missing
+    for (let i=0; i<rca_sections.length - 1; i++) {
+        if (isBetween(issue.body, rca_sections[i], "_No response_", rca_sections[i+1])) {
+            reopenIssue = true;
         }
     }
 
